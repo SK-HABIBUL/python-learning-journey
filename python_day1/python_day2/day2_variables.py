0a588a1ca5639eb8b33a258c4a8092159e23eb49
SK-Habibul <Skhabibul999@gmail.com>
2025-12-10 00:12:43 +0530

    Day 2: Variables and Data Types (Python Code)

---
+++ python_day1/python_day2/day2_variables.py
@@ -0,0 +1,21 @@
+# Day 2: Variables & Data Types
+
+name = "Habib"
+age = 20
+height = 5.8
+is_student = True
+
+# Data structures
+fruits = ["apple", "banana", "orange"]     # list
+point = (10, 20)                           # tuple
+person = {"name": "Habib", "course": "Python"}   # dict
+unique_numbers = {1, 2, 3, 3}              # set (duplicates removed)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(fruits))
+print(type(point))
+print(type(person))
+print(type(unique_numbers))
